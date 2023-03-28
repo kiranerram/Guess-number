f3e3b04aa7170ea74535b2dc0b81a03ed4b2334f
kiranerram <54358256+kiranerram@users.noreply.github.com>
2023-03-28 20:05:14 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,31 @@
+// random value generated
+var y = Math.floor(Math.random() * 10 + 1);
+
+playername=                          ;
+// counting the number of guesses
+// made for correct Guess
+var guess = 1;
+  
+function submit(){
+  
+// number guessed by user     
+var x = document.getElementById("guessField").value;
+
+if(x == y)
+{    
+   
+
+}
+else if(x > y) /* if guessed number is greater
+               than actual number*/ 
+{    
+  
+}
+else
+{
+   
+}
+}
+function playAgain(){
+    y = ;
+}
